d600d5f927999ffeedca7ee349b278582c82f0ef
Ostrovski <vladimir_ostrovski@web.de>
2019-07-31 19:07:14 +0200

    work on readability

---
+++ research.R
@@ -0,0 +1,100 @@
+source("distance.R")
+source("asymptotic_test.R")
+source("bootstrap_test.R")
+source("data_sets.R")
+source("simulation.R")
+source("size.R")
+source("power.R")
+
+# We simulate the test power at the Hardy Weinberg equilibrium points, 
+# which rand distribution (distribution of alleles) equals the given data set.
+# The power is simulated for different values of tolerance parameter epsilon to
+# obtain the overview of the II. type error at the Hardy Weinberg equilibrium
+# conditionally on the alleles distribution.
+# Note that the II. type error equals 1-power.
+
+# number of samples to estimate test power
+nSamples=1000
+
+# Selector contains logical values, which select the evaluation 
+# for the different tests as follows: 
+# selector[1] for the asymptotic test based on the conditional distance
+# selector[2] for the bootstrap test based on the conditional distance
+# selector[3] for the asymptotic test based on the minimum distance
+# selector[4] for the bootstrap test based on the minimum distance
+# Note that the power calculation needs a very long time (days) for the bootstrap tests.
+# Power calculation uses parallel processing. Thus a strong parallel hardware is advantageous.
+selector=c(TRUE,FALSE,FALSE,FALSE)
+
+#power at sample1
+sizeSample1=matrix(data=NA, nrow=10, ncol=5)
+colnames(sizeSample1)=c("eps","asy_cond","bst_cnd","asy_min","bst_min")
+
+sizeSample1[1,]=powerAtPoint(example1,0.12,nSamples,selector)
+sizeSample1[2,]=powerAtPoint(example1,0.10,nSamples,selector)
+sizeSample1[3,]=powerAtPoint(example1,0.09,nSamples,selector)
+sizeSample1[4,]=powerAtPoint(example1,0.08,nSamples,selector)
+sizeSample1[5,]=powerAtPoint(example1,0.07,nSamples,selector)
+
+write.table(sizeSample1, "sizeSample1.txt")
+
+# power at sample2
+sizeSample2=matrix(data=NA, nrow=10, ncol=5)
+colnames(sizeSample2)=c("eps","asy_cond","bst_cnd","asy_min","bst_min")
+
+sizeSample2[1,]=powerAtPoint(example2,0.12,nSamples,selector)
+sizeSample2[2,]=powerAtPoint(example2,0.10,nSamples,selector)
+sizeSample2[3,]=powerAtPoint(example2,0.09,nSamples,selector)
+sizeSample2[4,]=powerAtPoint(example2,0.08,nSamples,selector)
+sizeSample2[5,]=powerAtPoint(example2,0.07,nSamples,selector)
+
+write.table(sizeSample2, "sizeSample2.txt")
+
+# power at sample3
+sizeSample3=matrix(data=NA, nrow=10, ncol=5)
+colnames(sizeSample3)=c("eps","asy_cond","bst_cnd","asy_min","bst_min")
+ 
+sizeSample3[1,]=powerAtPoint(example3,0.018,nSamples,selector)
+sizeSample3[2,]=powerAtPoint(example3,0.016,nSamples,selector)
+sizeSample3[3,]=powerAtPoint(example3,0.014,nSamples,selector)
+sizeSample3[4,]=powerAtPoint(example3,0.012,nSamples,selector)
+sizeSample3[5,]=powerAtPoint(example3,0.010,nSamples,selector)
+
+
+write.table(sizeSample3, "sizeSample3.txt")
+
+
+# power at sample4
+sizeSample4=matrix(data=NA, nrow=10, ncol=5)
+colnames(sizeSample4)=c("eps","asy_cond","bst_cnd","asy_min","bst_min")
+
+sizeSample4[1,]=powerAtPoint(example4,0.06,nSamples,selector)
+sizeSample4[2,]=powerAtPoint(example4,0.05,nSamples,selector)
+sizeSample4[3,]=powerAtPoint(example4,0.04,nSamples,selector)
+sizeSample4[4,]=powerAtPoint(example4,0.03,nSamples,selector)
+
+
+write.table(sizeSample4, "sizeSample4.txt")
+
+# Next we investigate if the type II error is sensitive to the allele distribution.
+# For this porpose we estimate the allele distribution from the counting data.
+# Then the random samples from the allele distribution are generated
+
+# sensi_example1=powerSensitivity(tab=example1,eps=0.1,nSamples = 1000,
+#                                 selector = c(TRUE,FALSE,TRUE,FALSE),
+#                                 nSimulation = 1000)
+# write.table(sensi_example1,"sensi_example1.txt")
+# 
+# sensi_example2=powerSensitivity(tab=example2,eps=0.1,nSamples = 1000,
+#                                 selector = c(TRUE,FALSE,TRUE,FALSE),
+#                                 nSimulation = 1000)
+# write.table(sensi_example2,"sensi_example2.txt")
+# 
+# sensi_example3=powerSensitivity(tab=example3,eps=0.016,nSamples = 1000, 
+#                                 selector = c(TRUE,FALSE,TRUE,FALSE),
+#                                 nSimulation = 1000)
+# write.table(sensi_example3,"sensi_example3.txt")
+# 
+# sensi_example4=powerSensitivity(tab=example4,eps=0.05,nSamples = 1000, 
+#                                 selector = c(TRUE,FALSE,TRUE,FALSE))
+# write.table(sensi_example4,"sensi_example4.txt")
