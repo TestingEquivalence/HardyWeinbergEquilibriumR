57a59f44669e8bf07b224cc69369bd02a2b63a3e
Ostrovski <vladimir_ostrovski@web.de>
2019-09-21 10:56:11 +0200

    delete rare data

--- data_sets.R
+++ data_sets.R
@@ -20,20 +20,20 @@ example2 = matrix(data=c(2, 0, 0, 0,
                   nrow=4, ncol=4, byrow=TRUE)
 
 # 3. example
-# Genotype frequency data at Rhesus locus.
+# Genotype frequency data at Rhesus locus. 
+# Alle 7 and 8 are delited from the matrix because 
+# rarely observed (14 and 2 times)
 # Cavalli-Sforza, L. L. and Bodmer, W. F. (1971). 
 # The Genetics of Hutman Populations. San Francisco: W. H. Freeman
 
-example3= matrix(data=c(1236, 0, 0, 0, 0, 0, 0, 0, 0,
-                        120, 3, 0, 0, 0, 0, 0, 0, 0,
-                        18, 0, 0, 0, 0, 0, 0, 0, 0,
-                        982, 55, 7, 249, 0, 0, 0, 0, 0,
-                        32, 1, 0, 12, 0, 0, 0, 0, 0,
-                        2582, 132, 20, 1162, 29, 1312, 0, 0, 0,
-                        6, 0, 0, 4, 0, 4, 0, 0, 0,
-                        2, 0, 0, 0, 0, 0, 0, 0, 0,
-                        115, 5, 2, 53, 1, 149, 0, 0, 4),
-                 nrow=9, ncol=9, byrow=TRUE)
+example3= matrix(data=c(1236, 0, 0, 0, 0, 0, 0,
+                        120, 3, 0, 0, 0, 0, 0, 
+                        18, 0, 0, 0, 0, 0, 0, 
+                        982, 55, 7, 249, 0, 0, 0, 
+                        32, 1, 0, 12, 0, 0, 0, 
+                        2582, 132, 20, 1162, 29, 1312, 0, 
+                        115, 5, 2, 53, 1, 149, 4),
+                 nrow=7, ncol=7, byrow=TRUE)
 
 # 4. example
 # HFE genotype of the whole population referred for a personal or family
