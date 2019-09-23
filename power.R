acf7b6359e8052e9030d23ffb18931e4f83c6678
Ostrovski <vladimir_ostrovski@web.de>
2019-09-23 08:41:27 +0200

    restore previous bootstrap tests

--- power.R
+++ power.R
@@ -4,6 +4,40 @@ source("bootstrap_test.R")
 source("data_sets.R")
 source("simulation.R")
 
+closeRandomPoint<-function(tab, eps, distance){
+  n=sum(tab)
+  tab=tab/n
+  
+  repeat{
+    rtab=as.vector(tab)
+    v=rmultinom(n=1,size=n,prob=rtab)
+    v=v/n
+    m=matrix(data=v,nrow=nrow(tab), ncol=ncol(tab))
+    t= distance(m)
+    if (t>eps) return(m)
+  }
+  
+}
+
+closeBoundaryPoint<-function(i,tab,eps,distance){
+  p=closeRandomPoint(tab,eps,distance)
+  n=sum(tab)
+  tab=tab/n
+  
+  if (identical(distance,cond_l2)){
+    q=p2triangle(startValue(tab))
+  }
+  
+  if (identical(distance,min_l2)){
+    q=min_l22(tab)$par
+    q=p2triangle(q)
+  }
+  
+  res=linearBoundaryPoint(p,q,eps,distance)
+  return(res)
+}
+
+
 
 powerAtBoundaryConditionalAsympt<-function(tab, nSamples, cl){
   i=c(1:100)
